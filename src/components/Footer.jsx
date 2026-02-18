c62fa3b705e051eb3d2fc972e27c280f5dd05cd0
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2026-02-18 21:31:17 +0530

    Portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-import { socialLinks } from "../constants";
+
 
 const Footer = () => {
   return (
@@ -12,17 +12,7 @@ const Footer = () => {
           © 2026 <strong>Jaskaran Guru</strong>. All rights reserved.
         </p>
 
-        <div className='flex gap-3 justify-center items-center'>
-          {socialLinks.map((link) => (
-            <Link key={link.name} to={link.link} target='_blank'>
-              <img
-                src={link.iconUrl}
-                alt={link.name}
-                className='w-6 h-6 object-contain'
-              />
-            </Link>
-          ))}
-        </div>
+        
       </div>
     </footer>
   );
