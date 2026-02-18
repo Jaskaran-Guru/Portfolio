424359c6b41f5e0009785d224d988ed603a17496
Jaskaran-Guru <gurujaskaran2006@gmail.com>
2026-02-18 21:45:47 +0530

    Portfolio

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,12 +2,23 @@ import { Link } from "react-router-dom";
 
 const CTA = () => {
   return (
-    <section className='cta'>
-      <p className='cta-text'>
-        Have a project in mind? <br className='sm:block hidden' />
-        Let’s build something together!
-      </p>
-     
+    <section className="cta">
+      <div className="max-w-6xl mx-auto flex flex-col sm:flex-row items-center justify-between gap-6">
+        
+        <p className="cta-text text-center sm:text-left">
+          Have a project in mind?{" "}
+          <br className="sm:block hidden" />
+          Let’s build something together!
+        </p>
+
+        <Link
+          to="/contact"
+          className="bg-purple-600 hover:bg-purple-700 text-white px-2 py-1 rounded-lg font-semibold transition"
+        >
+          Contact
+        </Link>
+
+      </div>
     </section>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { socialLinks } from "../constants";
 
 
 
@@ -12,7 +13,17 @@ const Footer = () => {
           © 2026 <strong>Jaskaran Guru</strong>. All rights reserved.
         </p>
 
-        
+        <div className='flex gap-3 justify-center items-center'>
+          {socialLinks.map((link) => (
+            <Link key={link.name} to={link.link} target='_blank'>
+              <img
+                src={link.iconUrl}
+                alt={link.name}
+                className='w-6 h-6 object-contain'
+              />
+            </Link>
+          ))}
+        </div>
       </div>
     </footer>
   );
